5f6828c5960fbe27e109a44935d2af1940954aec
linyuanda <linyuanda@linyuanda.COM>
2025-04-05 14:36:01 +0800

    Python课程

--- 第六章面向对象编程/微实例1.3类的构造方法和析构方法/微实例1.3.py
+++ 第六章面向对象编程/微实例1.3类的构造方法和析构方法/微实例1.3.py
@@ -23,3 +23,15 @@ my_car.run()
 """
 2.析构法
 """
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    # 析构方法
+    def __del__(self):
+        print("释放{}这个对象的资源".format(self.name))
+
+# 实例化对象
+p = Person("小明",18)
+# 释放对象
+del p
