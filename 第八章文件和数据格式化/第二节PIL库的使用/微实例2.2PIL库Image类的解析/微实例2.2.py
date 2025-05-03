e5eacde23652161b56f34b5253ef501d4ac80cde
linyuanda <linyuanda@linyuanda.COM>
2025-05-03 18:03:32 +0800

    Python课程

---
+++ 第八章文件和数据格式化/第二节PIL库的使用/微实例2.2PIL库Image类的解析/微实例2.2.py
@@ -0,0 +1,29 @@
+# 微实例8.2.2PIL库Image类的解析
+from PIL import Image
+"""
+在PIL中，任何一个图像文件都可以用Image对象表示Image类的图像读取和创建方法。
+Image.open(filename)：根据参数加载图像文件
+Image.new(mode, size, color)：根据给定参数创建一个新的图像
+Image.open(StringIO.StringIO(buffer))：从字符串中获取图像
+Image.frombytes(mode, size, data)：根据像素点data创建图像
+Image.verify()：对图像文件完整性进行检查，返回异常
+"""
+
+"""
+要加载一个图像文件，最简单的形式如下，之后所有操作对im起作用
+"""
+im = Image.open("birdnest.jpg")
+"""
+Image类有4个处理图片的常用属性：
+Image.format：标识图像格式或来源，如果图像不是从文件读取，值是None
+Image.mode：图像的色彩模式，"L"灰度图像、"RGB"真彩色图像、"CMYK"出版图像
+Image.size：图像宽度和高度，单位是像素（px），返回值是二元元组（tuple）
+Image.palette：调色板属性，返回一个ImagePalette类型
+"""
+print(im.format, im.size, im.mode)
+"""
+Image还能读取序列的图像文件，包括GIF、FLI、FLC、TIFF等格式文件
+Image类的序列图像操作方法：
+Image.seek(frame)：跳转并返回图像中的指定帧
+Image.tell()：返回当前帧的序号
+"""
