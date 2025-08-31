76214b4acb0a246024defd41093d2b3b63237e91
linyuanda <linyuanda@linyuanda.com>
2025-08-31 12:41:02 +0800

    Python课程

--- 第十章网络爬虫和自动化/第一节requests库的使用/微实例1.3获取一个HTML通用代码/微实例1.3.py
+++ 第十章网络爬虫和自动化/第一节requests库的使用/微实例1.3获取一个HTML通用代码/微实例1.3.py
@@ -1,4 +1,12 @@
 # 微实例1.3获取一个HTML通用代码
+"""
+json()方法能够在HTTP响应内容中解析存在的JSON数据，这将带来解析HTTP的便利。
+raise_for_status()方法能在非成功响应后产生异常，
+即只要返回的请求状态status_code不是200，
+这个方法会产生一个异常，用于try…except语句。
+使用异常处理语句可以避免设置一堆复杂的if语句，
+只需要在收到响应调用这个方法，就可以避开状态字200以外的各种意外情况。
+"""
 import requests
 def get_html_text(u):
     try:
@@ -10,3 +18,15 @@ def get_html_text(u):
         return ""
 url = "https://www.baidu.com"
 print(get_html_text(url))
+"""
+HTTP 协议定义了客户端与服务器交互的不同方法，最基本的方法是GET和POST。
+顾名思义，GET可以根据某链接获得内容，
+POST用于发送内容。
+然而，GET也可以向链接提交内容
+1.GET 方式可以通过URL提交数据，待提交数据是URL 的一部分；
+采用POST方式，待提交数据放置在HTML HEADER内；
+2.GET方式提交的数据最多不超过1024字节，POST没有对提交内容的长度限制。
+3.安全性问题。如1所述，使用GET时参数会显示在URL中，而POST不会。
+所以，如果这些数据是非敏感数据，那么使用GET；
+如果提交数据是敏感数据，建议采用POST方式。
+"""
